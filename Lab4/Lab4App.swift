ec53aa26244f83237102ed4077c90183366fe88f
Zachary <zach.lee96@gmai.com>
2024-02-17 03:41:57 -0700

    Initial Commit

---
+++ Lab4/Lab4App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab4App.swift
+//  Lab4
+//
+//  Created by Zachary Lee on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
